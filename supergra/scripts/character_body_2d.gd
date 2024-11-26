92b857cf753087d84d093f3a2b4d081d2eb01e8d
Pequu <rzymekrz@gmail.com>
2024-11-26 15:30:46 +0100

    komentearz

--- supergra/scripts/character_body_2d.gd
+++ supergra/scripts/character_body_2d.gd
@@ -8,7 +8,6 @@ func _physics_process(delta: float) -> void:
 
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x := Input.get_axis("left", "right")
 	var direction_y := Input.get_axis("up", "down")
 	if direction_x:
